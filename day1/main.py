c4a64b4741db2ecf578e76a13ad7ca20f04d0272
Tobias Heyer <saibot133@web.de>
2025-12-01 20:03:45 +0100

    [d1] solved p1

--- day1/main.py
+++ day1/main.py
@@ -14,17 +14,28 @@ class Data:
 
 def extract_data() -> list[Data]:
     result = []
-    #with open(r"input") as inp_file:
-    with open(r"test_input") as inp_file:
+    with open(r"input") as inp_file:
+    #with open(r"test_input") as inp_file:
         for l in inp_file:
             result.append(Data(l[0], int(l[1:len(l)-1])))
     return result
 
     
-def solve():
-    pass
+def solve(instruction_list:list[Data])->int:
+    value = 50
+    zero_counter = 0
+    for instruction in instruction_list:
+        if instruction.direction == "L":
+            value = (value-instruction.distance) % 100
+        else :
+            value = (value+instruction.distance) % 100
+
+        if value == 0:
+            zero_counter += 1
+    return zero_counter
+
 
 ##
 # test input solution is '3' (number of times dial points at 0)
 if __name__ == '__main__':
-    print(extract_data())
+    print(solve(extract_data()))
